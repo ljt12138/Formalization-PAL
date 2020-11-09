7aa658aa9eee078076459811c12a48a7292b094f
Li Jiatu <ljt12138@qq.com>
2020-11-09 21:11:36 +0800

    [Code] prove complete extension for pal

--- src/pal/henkin_model.lean
+++ src/pal/henkin_model.lean
@@ -47,7 +47,7 @@ set of sentences Δ = {¬ψ} ∪ {α | □ᵢα ∈ s}.
   The Main Lemma will be explained more clearly in section 3.
 -/
 
-/-                ----1. Definitions and Lemmas----                     -/
+/-               ---- 1. Definitions and Lemmas ----                    -/
 
 def decider (α agent : Type) := sentence α agent → Prop
 @[simp] def consistent_set {α agent : Type} (d : decider α agent) :=
@@ -335,24 +335,81 @@ end
 private def ext {α agent : Type} (d : decider α agent) (φ : sentence _ _) := 
   λ ψ, ψ = φ ∨ d ψ
 
+lemma ext_origin {α agent : Type} {d : decider α agent} {φ : sentence α agent} :
+  ∀ ψ, d ψ → (ext d φ) ψ := 
+begin
+  intros, right, exact a
+end
+
+lemma ext_new {α agent : Type} {d : decider α agent} {φ : sentence α agent} : 
+  ext d φ φ :=
+begin
+  left, refl
+end
+
 -- firstly, show that Δ ∪ {φ} ⊢ ⊥ implies that Δ ⊢ ¬φ, 
 --  then, in the case ¬φ ∈ Γ and φ ∈ Γ', we have Δ ⊢ φ and Δ ⊢ ¬φ, hence Δ ⊢ ⊥, 
 --  contradiction
 
-lemma dsingle_extension {α agent : Type} (d : decider α agent) (φ : sentence _ _) :
-  consistent_set d → ∃ d', consistent_set d' ∧ (∀ ψ, d ψ → d' ψ) ∧ (d' φ ∨ d' (φ↣⊥)) :=
+private def single_extend {α agent : Type} (d : decider α agent) (φ : sentence _ _) := 
+begin 
+  classical,
+  exact if (consistent_set (ext d φ)) then ext d φ else ext d (φ ↣ ⊥)
+end
+
+private def henkin_enum {α agent : Type} [encodable (sentence α agent)]
+        (d₀ : decider α agent) : ℕ → decider α agent 
+| 0         := d₀
+| (n + 1)   := let d := henkin_enum n in 
+                 match encodable.decode (sentence α agent) n with 
+                 | none        := d
+                 | some φ      := single_extend d φ
+                 end
+
+lemma extend_preserve {α agent : Type} (d : decider α agent) (φ ψ : sentence _ _) :
+  d ψ → consistent_set d → single_extend d φ ψ :=
 begin
-  intros h₁, 
+  intros h₁ h₂,
+  cases em (consistent_set (ext d φ)), 
+  { 
+    simp [single_extend, h], cases em (φ = ψ),
+    { rewrite h_1, apply ext_new },
+    { apply ext_origin, exact h₁ } 
+  },
+  {
+    simp [single_extend, h], cases em (φ↣⊥ = ψ),
+    { rewrite h_1, apply ext_new },
+    { apply ext_origin, exact h₁ } 
+  }
+end
+
+lemma extend_new {α agent : Type} (d : decider α agent) (φ : sentence _ _) :
+  single_extend d φ φ ∨ single_extend d φ (φ↣⊥) :=
+begin
+  cases em (consistent_set (ext d φ)),
+  { simp [single_extend, h], left, apply ext_new },
+  { simp [single_extend, h], right, apply ext_new }
+end
+
+lemma dsingle_extension {α agent : Type} (d : decider α agent) (φ : sentence _ _) :
+  consistent_set d → let d' := single_extend d φ 
+    in consistent_set d' ∧ (∀ ψ, d ψ → d' ψ) ∧ (d' φ ∨ d' (φ↣⊥)) :=
+begin
+  intros h₁,
   cases classical.em (consistent_set (ext d φ)),
   { -- d ∪ {φ} is consistent 
-    existsi (ext d φ), split, exact h, split,
-    { intros ψ h₂, simp [ext], right, exact h₂ },
-    { simp [ext] }
+    split, 
+    { simp [single_extend, h], exact h },
+    { 
+      simp [single_extend, h, ext], split,
+      { intros ψ h', right, exact h' },
+      { left, left, refl }
+    }
   },
   { -- d ∪ {φ} is inconsistent
-    existsi (ext d (φ↣⊥)), split, 
+    split, 
     { 
-      unfold consistent_set, 
+      simp [single_extend, h₁, h], 
       intros Γ h₂ contra₁,
       apply h, intros Γ' h₃ contra₂,
       cases em (φ↣⊥ ∈ Γ),
@@ -399,22 +456,223 @@ begin
       }
     }, 
     { 
-      split, intros, simp [ext], right, exact a,
-      simp [ext]
+      simp [single_extend, h], split,
+      { apply ext_origin },
+      { right, apply ext_new }
     }
   }
 end
 
-theorem dcomplete_extension {α agent : Type} :
+private def prefix_decider {α agent : Type} [encodable (sentence α agent)] 
+  (d : decider α agent) (n : ℕ) := ∀ φ, encodable.encode φ < n →  d φ ∨ d (φ ↣ ⊥)
+ 
+lemma henkin_enum_consistent {α agent : Type} [encodable (sentence α agent)]
+  (d₀ : decider α agent) (h : consistent_set d₀) (n : ℕ) : 
+    consistent_set (henkin_enum d₀ n) :=
+begin
+  induction n with n ih,
+  { exact h },
+  {
+    unfold henkin_enum, 
+    have ht : ∃ pφ, pφ = encodable.decode (sentence α agent) n, 
+    { existsi _, refl }, 
+    cases ht with φ ref, rewrite ← ref, cases φ,
+    { simp [henkin_enum], exact ih },
+    { simp [henkin_enum], exact (dsingle_extension _ φ ih).left }
+  }
+end
+
+lemma henkin_enum_prefix {α agent : Type} [encodable (sentence α agent)]
+  (d₀ : decider α agent) (h : consistent_set d₀) (n : ℕ) : 
+    prefix_decider (henkin_enum d₀ n) n := 
+begin
+  induction n with n ih,
+  { simp [prefix_decider, henkin_enum] },
+  {
+    simp [prefix_decider, henkin_enum], 
+    have ht : ∃ pφ, pφ = encodable.decode (sentence α agent) n, 
+    { existsi _, refl },
+    cases ht with pφ ref, rewrite ← ref, cases pφ with φ,
+    { -- [φ] = n
+      simp [henkin_enum], intros ψ a, 
+      have ht : encodable.encode ψ ≤ n := by omega,
+      cases lt_or_eq_of_le ht with h₁ h₁, 
+      { apply ih, exact h₁ },
+      { rewrite ← h₁ at ref, simp at ref, exfalso, exact ref }
+    },
+    { -- [φ] < n
+      simp [henkin_enum], intros ψ a,
+      have ht : encodable.encode ψ ≤ n := by omega,
+      cases lt_or_eq_of_le ht with h₁ h₁, 
+      {
+        unfold prefix_decider at ih,
+        cases ih ψ h₁ with h₂ h₂, 
+        { 
+          left, apply extend_preserve, exact h₂, 
+          apply henkin_enum_consistent, exact h,   
+        },
+        {
+          right, apply extend_preserve, exact h₂,
+          apply henkin_enum_consistent, exact h
+        }
+      },
+      {
+        rewrite ← h₁ at ref, simp at ref, rewrite ref, 
+        apply extend_new
+      }
+    }
+  }
+end 
+
+lemma henkin_enum_subset {α agent : Type} [encodable (sentence α agent)] 
+  (d₀ : decider α agent) (h : consistent_set d₀) (n m : ℕ) (φ : sentence α agent) : 
+    (n ≤ m) → (henkin_enum d₀ n) φ → (henkin_enum d₀ m) φ :=
+begin
+  intros h₁ h₂,
+  induction m with m ih,
+  { have ht : n = 0 := by omega, rewrite ht at h₂, exact h₂ },
+  {
+    cases lt_or_eq_of_le h₁ with h₃ h₃,
+    {
+      simp [henkin_enum],
+      have ht : ∃ pφ, pφ = encodable.decode (sentence α agent) m, 
+      { existsi _, refl },
+      cases ht with pφ h₃, rewrite ← h₃, cases pφ with ψ,
+      { simp [henkin_enum], apply ih, omega},
+      { 
+        simp [henkin_enum], apply extend_preserve, apply ih, omega, 
+        apply henkin_enum_consistent, exact h 
+      }
+    },
+    { rewrite ← h₃, exact h₂ }
+  }
+end
+
+lemma henkin_enum_em {α agent : Type} [encodable (sentence α agent)]
+  (d₀ : decider α agent)(φ : sentence α agent) :
+    henkin_enum d₀ (encodable.encode φ + 1) φ 
+      ∨ henkin_enum d₀ (encodable.encode φ + 1) (φ↣⊥) :=
+begin
+  simp [henkin_enum],
+  cases em (consistent_set (ext (henkin_enum d₀ (encodable.encode φ)) φ)),
+  { simp [single_extend, h], left, apply ext_new },
+  { simp [single_extend, h], right, apply ext_new }
+end
+    
+def henkin_union {α agent : Type} [encodable (sentence α agent)]
+  (d₀ : decider α agent) (φ : sentence α agent) :=
+    henkin_enum d₀ ((encodable.encode φ) + 1) φ
+
+lemma henkin_union_subset {α agent : Type} [encodable (sentence α agent)]
+  (d₀ : decider α agent) (h : consistent_set d₀) (φ : sentence _ _) :
+    d₀ φ → henkin_union d₀ φ :=
+begin
+  intros h₁, unfold henkin_union, 
+  apply henkin_enum_subset _ h 0, simp, 
+  simp [henkin_enum], exact h₁
+end
+
+def maximum_encoding {α agent : Type} [encodable (sentence α agent)] 
+  : list (sentence α agent) → ℕ 
+| []     := 0
+| (φ::Γ) := max (encodable.encode φ + 1) (maximum_encoding Γ) 
+
+lemma henkin_union_Γ {α agent : Type} [encodable (sentence α agent)]
+  (d₀ : decider α agent) (h : consistent_set d₀) (Γ : list (sentence α agent)) :
+    (∀ φ, φ ∈ Γ → henkin_union d₀ φ) → 
+    (∀ φ, φ ∈ Γ → henkin_enum d₀ (maximum_encoding Γ) φ) :=
+begin
+  intros h₁ φ h₂,
+  induction Γ with ψ Γ ih,
+  { simp at h₂, exfalso, exact h₂ },
+  { 
+    simp [maximum_encoding], simp at h₂, cases h₂,
+    {
+      rewrite h₂ at *,
+      have ht := h₁ ψ (list.mem_cons_self _ _), simp [henkin_union] at ht,
+      apply henkin_enum_subset, exact h, apply le_max_left, exact ht
+    },
+    { 
+      apply henkin_enum_subset, exact h, apply le_max_right, apply ih,
+      intros ρ h₃, apply h₁, simp, right, exact h₃, exact h₂
+    }
+  }
+end
+
+lemma henkin_union_consistent {α agent : Type} [encodable (sentence α agent)]
+  (d₀ : decider α agent) (h : consistent_set d₀) : 
+    consistent_set (henkin_union d₀) :=
+begin
+  intros Γ h₁ contra,
+  apply (henkin_enum_consistent d₀ h),
+  apply henkin_union_Γ, exact h, 
+  exact h₁, exact contra
+end
+
+lemma henkin_enum_agree {α agent : Type} [encodable (sentence α agent)] 
+  (d₀ : decider α agent) (h : consistent_set d₀) (φ : sentence α agent) (n : ℕ) :
+    henkin_enum d₀ n φ → henkin_enum d₀ (encodable.encode φ + 1) φ :=
+begin
+  intros h₁, classical, by_contra h₂,
+  have ht : henkin_enum d₀ (encodable.encode φ + 1) (φ ↣ ⊥),
+  { 
+    have ht' := henkin_enum_em d₀ φ,
+    cases ht', exfalso, exact h₂ ht',
+    exact ht'
+  },
+  have ht₁ := henkin_enum_subset d₀ h n (max n (encodable.encode φ + 1)) φ (le_max_left _ _) h₁, 
+  have ht₂ := henkin_enum_subset d₀ h _ (max n (encodable.encode φ + 1)) (φ↣⊥) (le_max_right _ _) ht, 
+  apply henkin_enum_consistent d₀ h (max n (encodable.encode φ + 1)) [φ, φ↣⊥],
+  intros ψ, 
+  { intros a, simp at a, cases a, repeat {rewrite a, assumption} },
+  {
+    unfold conjunction, 
+    apply uncurry, existsi proof.ax2' _, prover 
+  }
+end
+
+lemma henkin_union_maximal {α agent : Type} [encodable (sentence α agent)]
+  (d₀ : decider α agent) (h : consistent_set d₀) : 
+    ∀ φ, henkin_union d₀ φ ∨ henkin_union d₀ (φ ↣ ⊥) :=
+begin
+  intros φ, 
+  cases em (henkin_union d₀ φ) with h₁ h₁, 
+  { exact or.inl h₁ },
+  { 
+    right, unfold henkin_union at *, 
+    have ht₂ : henkin_enum d₀ (encodable.encode φ + 1) (φ ↣ ⊥),
+    {  
+      have ht₃ := henkin_enum_em d₀ φ,
+      cases ht₃, 
+      { exfalso, exact h₁ ht₃ },
+      { exact ht₃ }
+    },
+    apply henkin_enum_agree, repeat {assumption}
+  }
+end
+
+lemma henkin_union_consistent_decider {α agent : Type} [encodable (sentence α agent)]
+  (d₀ : decider α agent) (h : consistent_set d₀) :
+    consistent_decider (henkin_union d₀) :=
+begin
+  unfold consistent_decider, split,
+  { apply henkin_union_consistent, exact h }, 
+  { apply henkin_union_maximal, exact h }
+end
+
+theorem dcomplete_extension {α agent : Type} [encodable (sentence α agent)] :
   ∀ d : decider α agent, consistent_set d → 
     ∃ d' : decider_world α agent, (∀ φ, d φ → decides d' φ) :=
 begin
-  sorry
+  intros d₀ h, 
+  have ht := henkin_union_consistent_decider d₀ h, 
+  existsi (decider_world.mk (henkin_union d₀) ht),
+  intros φ h₁, simp, apply henkin_union_subset, exact h, exact h₁
 end
 
 /-                ----4. Correctness of Henkin model----              -/
 
-theorem henkin_correctness {α agent : Type} :
+theorem henkin_correctness {α agent : Type} [encodable (sentence α agent)]:
   ∀ φ, static φ → ∀ s : decider_world α agent, (decides s φ ↔ ⦃s⦄ ⊨ φ) :=
 begin
   intros φ st, 
